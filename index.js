4a2d270f8006771f5ed7f25b1d40c7d3e8af37dd
Sufiyan <112167769+Sufiyanzabaq@users.noreply.github.com>
2023-09-08 21:03:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const menuBtn = document.querySelector(".ham")
 const closeBtn = document.querySelector(".close-icon")
 const mobileMenu = document.querySelector(".mobile-menu")
+const dropdown = document.querySelector(".dropdown")
+const overFlow = document.querySelector(".overflow")
 
 
 menuBtn.addEventListener("click", () =>{
     mobileMenu.style.transform = "translate(0)"
+    overFlow.style.display = "block"
 })
 closeBtn.addEventListener("click", () => {
     mobileMenu.style.transform = "translate(100%)"
+    overFlow.style.display = "none"
 })
+
